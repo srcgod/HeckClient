4fd39cbec10dca7b26a158ee4adbf8b63bf0d366
srgd <elenapolekova23@gmail.com>
2024-08-08 20:48:57 +0300

    fix nametag

--- src/main/java/net/kyc/client/init/Fonts.java
+++ src/main/java/net/kyc/client/init/Fonts.java
@@ -3,6 +3,6 @@ package net.kyc.client.init;
 import net.kyc.client.impl.font.VanillaTextRenderer;
 
 public class Fonts {
-
+    //
     public static final VanillaTextRenderer VANILLA = new VanillaTextRenderer();
 }
